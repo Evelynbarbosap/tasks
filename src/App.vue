4e4c97004a14af2ab410ccc7525485dc076b084f
Evelyn Barbosa <evelynbarbosap64@gmail.com>
2022-10-30 22:23:31 -0300

    Cria components e listagem de todos do bd com vuex e axios

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+            <TodoSpinner v-if="loading" />
+
+            <template v-else>
+                <TodoFormAdd />
+
+                <TodoItems />
+
+                <TodoEmpty />
+            </template>
+        </div>
+    </div>
+</template>
+<script>
+import TodoSpinner from '@/components/TodoSpinner';
+import TodoFormAdd from '@/components/TodoFormAdd';
+import TodoItems from '@/components/TodoItems';
+import TodoEmpty from '@/components/TodoEmpty';
+import axios from 'axios'
+export default {
+    name: 'App',
+    components:{TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty},
+    data() {
+        return {
+            loading: false
+        }
+    },
+    created() {
+        this.loading = true
+        axios.get('http://localhost:3000/todos')
+        .then((response) => {
+            this.$store.commit('storeTodos', response.data)
+        })
+        .finally(() => {this.loading = false})
+    },
+}
+</script>
+
