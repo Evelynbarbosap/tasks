ff35b840cee701fe64a86af584df006510b12e08
Evelyn Barbosa <evelynbarbosap64@gmail.com>
2022-11-20 19:15:50 -0300

    Remove uma tarefa

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,14 @@ export default createStore({
       } else {
         state.todos.push(payload)
       }
+    },
+
+    deleteTodo(state, id){
+      const index = state.todos.findIndex(todo => todo.id === id)
+      if (index >= 0) {
+        state.todos.splice(index, 1)
+
+      }
     }
   },
   actions: {
@@ -37,6 +45,10 @@ export default createStore({
     updateTodo({commit}, {id, data}) {
       return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => commit('storeTodo', response.data));
     },
+
+    deleteTodo({commit}, id) {
+      return axios.delete(`http://localhost:3000/todos/${id}`).then(() => commit('deleteTodo', id));
+    }
 
   },
   modules: {
